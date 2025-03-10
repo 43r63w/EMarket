b233af4f6f85772111c7e60f4c6d4b74dcb6df3d
VLADYSLAV TKACHENKO <vt7098386@gmail.com>
2025-03-10 21:06:03 +0200

    Revert "Create second microserivce"
    This reverts commit 3193abc12d3116ee5a32df006f5c2c184210ae2d.

--- EMarket/src/Services/Basket.Api/GlobalUsing.cs
+++
@@ -1,7 +0,0 @@
-﻿global using MediatR;
-global using Carter;
-global using Mapster;
-global using Marten;
-global using BuildingBlocks.CQRS;
-global using FluentValidation;
-global using BuildingBlocks.Exceptions;
--- EMarket/src/Services/Basket.Api/Program.cs
+++ EMarket/src/Services/Basket.Api/Program.cs
@@ -1,44 +1,11 @@
-using Basket.Api;
-using Basket.Api.Models;
-using BuildingBlocks.Behaviors;
-using BuildingBlocks.Exceptions.Handlres;
-using System.Reflection;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddMarten(cfg =>
-{
-    cfg.Connection(builder.Configuration.GetConnectionString("Database")!);
-    cfg.Schema.For<Cart>().Identity(e => e.UserName);
-}).UseIdentitySessions();
-
-builder.Services.AddMediatR(config =>
-{
-    config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-
-    config.AddOpenBehavior(typeof(LoggingBehaviors<,>));
-    config.AddOpenBehavior(typeof(ValidationBehaviors<,>));
-});
-
-
-builder.Services.AddRedis(builder.Configuration);
-
-builder.Services.AddCarter();
-
-builder.Services.AddValidaitonSchema();
-
-builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
-
-app.UseExceptionHandler(opt => { });
-
-app.MapCarter();
-
 
 app.Run();
 
