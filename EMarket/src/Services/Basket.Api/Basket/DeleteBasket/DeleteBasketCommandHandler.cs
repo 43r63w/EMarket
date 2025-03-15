20da1264180fe7f70be53866f788b9298f25cdaf
VLADYSLAV TKACHENKO <vt7098386@gmail.com>
2025-03-15 18:43:28 +0200

    1

---
+++ EMarket/src/Services/Basket.Api/Basket/DeleteBasket/DeleteBasketCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿
+using Basket.Api.Data;
+
+namespace Basket.Api.Basket.DeleteBasket;
+
+internal sealed record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResponse>;
+
+internal sealed record DeleteBasketResponse(string message);
+
+internal sealed class DeleteBasketCommandHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResponse>
+{
+    private readonly IBasketRepository _basketRepository;
+
+    public DeleteBasketCommandHandler(IBasketRepository basketRepository)
+    {
+
+        _basketRepository = basketRepository;
+    }
+
+    public async Task<DeleteBasketResponse> Handle(
+        DeleteBasketCommand command,
+        CancellationToken cancellationToken)
+    {
+        await _basketRepository.DeleteBasketAsync(command.UserName);
+
+        return new DeleteBasketResponse($"{command.UserName} successfully deleted");
+    }
+}
